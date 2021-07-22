144f2cef3c6b5d81b77ffde51d6ca0f39d9ae111
Keng <rojwilai01@gmail.com>
2021-07-22 16:48:29 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -15,6 +15,57 @@ const barcodeDetector = new window.BarcodeDetector({
   formats: ['code_39', 'code_128', 'ean_8', 'ean_13', 'upc_a', 'upc_e']
 });
 
+const streamVideo = () => {
+
+  navigator.mediaDevices.getUserMedia({video: { pan: true, tilt: true, zoom: true }}).then(mediaStream => {
+    document.querySelector('video').srcObject = mediaStream;
+    const [track] = mediaStream.getVideoTracks();
+    const capabilities = track.getCapabilities();
+    const settings = track.getSettings();
+  
+    if (!('zoom' in settings)) {
+      return Promise.reject('Zoom is not supported by ' + track.label);
+    }
+
+    const input = document.querySelector('input[type="range"]');
+    console.log({input})
+    console.log({capabilities})
+    // Map zoom to a slider element.
+    input.min = capabilities?.zoom?.min || 1;
+    input.max = capabilities?.zoom?.max || 100;
+    input.step = capabilities.zoom.step;
+    input.value = settings.zoom;
+    input.oninput = function(event) {
+      track.applyConstraints({advanced: [ {zoom: event.target.value} ]});
+    }
+    // input.hidden = false;
+
+    
+
+    // track.applyConstraints({advanced: [ {zoom: 100} ]});
+  })
+
+  
+  // try {
+  //   // User is prompted to grant both camera and PTZ access in a single call.
+  //   // If camera doesn't support PTZ, it falls back to a regular camera prompt.
+  //   const stream = await navigator.mediaDevices.getUserMedia({
+  //     // Website asks to control camera PTZ as well without altering the
+  //     // current pan, tilt, and zoom settings.
+  //     video: { pan: true, tilt: true, zoom: true }
+  //   });
+  //   console.log({stream})
+  //   // Show camera video stream to user.
+  //   document.querySelector('#video').srcObject = stream;
+
+  // } catch (error) {
+  //   // User denies prompt or matching media is not available.
+  //   console.log(error);
+  // }
+}
+streamVideo()
+
+
 function App() {
   const webcamRef = useRef(null)
   const [imageSrc, setImageSrc] = useState('')
@@ -47,7 +98,9 @@ function App() {
 
   useEffect(() => {
     setInterval(capture, 1000);
-}, [capture]);
+  }, [capture]);
+
+
 
   return (
     <div className="App">
@@ -59,8 +112,12 @@ function App() {
           videoConstraints={{
               facingMode: 'environment'
           }}
+          onUserMedia={(props) => {
+            console.log(props)
+          }}
       />
       <img id="img" src={imageSrc} />
+      <input type="range"></input>
       {code}
     </div>
   );
