6230ca3d702f23012122218174707a8fd09c9d43
Keng <rojwilai01@gmail.com>
2021-07-22 15:24:21 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import {useRef} from 'react'
+import {useRef,useCallback,useEffect} from 'react'
 import Webcam from 'react-webcam';
 
 function App() {
   const webcamRef = useRef()
-  const imageSrc = webcamRef?.current?.getScreenshot();
-  const img = document.createElement('img');
-  img.src = imageSrc
-  if (!('BarcodeDetector' in window)) {
-      console.log(
-          'Barcode Detector is not supported by this browser.'
-      );
-  } else {
-      // create new detector
-      var barcodeDetector = new window.BarcodeDetector({
-          formats: ['code_39', 'codabar', 'ean_13']
-      });
 
-      barcodeDetector
-          .detect(img)
-          .then(barcodes => {
-              console.log({barcodes})
-          })
-          .catch(err => {
-              console.log({err})
-          });
-  }
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef?.current?.getScreenshot();
+    const img = document.createElement('img');
+    img.src = imageSrc
+    if (!('BarcodeDetector' in window)) {
+        console.log(
+            'Barcode Detector is not supported by this browser.'
+        );
+    } else {
+        // create new detector
+        var barcodeDetector = new window.BarcodeDetector({
+            formats: ['code_39', 'codabar', 'ean_13']
+        });
+  
+        barcodeDetector
+            .detect(img)
+            .then(barcodes => {
+                console.log({barcodes})
+            })
+            .catch(err => {
+                // console.log({err})
+            });
+    }
+  }, [webcamRef])
+
+  useEffect(() => {
+    setInterval(capture, 100);
+}, [capture]);
 
   return (
     <div className="App">
